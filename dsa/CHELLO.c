878acb3329bd4b43c3e63d06b15e5bb9e4e9239a
minhnbnt <minhnbnt@gmail.com>
2024-01-19 00:39:26 +0700

    refactor: unique way to solve CHELLO

---
+++ dsa/CHELLO.c
@@ -0,0 +1,27 @@
+/*
+Viết chương trình in ra màn hình dòng chữ:
+
+Hello PTIT.
+
+Input
+Không có dữ liệu vào
+
+Output
+Hello PTIT.
+*/
+
+const char *msg = "Hello PTIT.";
+
+int main(void) {
+
+	/* Just for fun, don't judge me. */
+	asm(".intel_syntax noprefix;");
+
+	asm("mov  rax, QWORD PTR msg[rip]");
+	asm("mov  rdi, rax");
+	asm("call puts");
+
+	asm(".att_syntax;");
+
+	return 0;
+}
